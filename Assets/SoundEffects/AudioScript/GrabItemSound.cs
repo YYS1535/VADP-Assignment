a80eafdc083ceb430573165c21ea836491a5915a
WOxiaoZHEwan <126762647+woxiaozhewan@users.noreply.github.com>
2024-12-15 23:05:42 +0800

    55%

--- Assets/SoundEffects/AudioScript/GrabItemSound.cs
+++ Assets/SoundEffects/AudioScript/GrabItemSound.cs
@@ -1,18 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class GrabItemSound : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public AudioSource grabSound;  // ????
+
+    private XRGrabInteractable grabInteractable;
+
+    void Awake()
     {
-        
+        grabInteractable = GetComponent<XRGrabInteractable>();
+        grabInteractable.selectEntered.AddListener(OnGrab);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnGrab(SelectEnterEventArgs args)
     {
-        
+        if (grabSound != null)
+        {
+            grabSound.Play();
+        }
+    }
+
+    private void OnDestroy()
+    {
+        grabInteractable.selectEntered.RemoveListener(OnGrab);  // ????
     }
 }
